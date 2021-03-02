72d26223a6d209eae63d62cd6246a62d603e6074
despresj <despresj@mail.gvsu.edu>
2021-03-02 10:18:57 -0500

    generalized function

--- Python/codeToText.py
+++ Python/codeToText.py
@@ -1,42 +1,44 @@
 #! /usr/bin/python3
 """
-This will collect all the code in a directory
-and create a finder friendly text file.
+TODO:
+test test test
 """
+
 import os
-import shutil
+from shutil import copyfile
 
-# select file extensions
-ext = (".py", ".ipynb", ".R", '.Rmd')
-# Path from which you want to obtian the code
-folder = ("")
-
-# any files you want to not include
-blacklist = ("")
-
-# directory you would like the combined file
-out_path = ""
-new_file_with_code = out_path + "Code.txt"
-
-
-codelist = []
-
-for foldername, subfolders, filenames in os.walk(folder):
-    for filename in filenames:
-      # Iterate through directory selecting file that meets conditions
-        if filename.endswith(ext) and not foldername.startswith(blacklist):
-            filename = foldername + "/" + filename 
-            
-            with open(os.path.join(filename), 'r') as filedata:
-              # make all code meeting conditions into a list
-                string = filedata.read()
-                codelist.append(string)
-                codelist.append('#' + '~_'*20)  
-          
-# write this list to a txt file
-with open(new_file_with_code, 'w') as filehandle:
-    for listitem in codelist:
-        filehandle.write('%s\n' % listitem)  
-# I prefer markdownfiles because they fit better into my workflow
-shutil.copyfile(new_file_with_code, out_path + "code.md")
-os.remove(new_file_with_code)
+def combinecode(folder, # Path from which you want to obtian the code
+                output_path = os.getcwd(),  
+                blacklist = (), # any files you want to not include
+                newfile = "codefile.txt",
+                mdfile = True, # get make file markdown
+                ext = (".py", ".ipynb", ".R", '.Rmd')): # select file extensions
+    
+    try:
+        output_path
+    except NameError:
+        output_path = folder
+    
+    codelist = []
+    for foldername, subfolders, filenames in os.walk(folder):
+        for filename in filenames:
+            if filename.endswith(ext) and not foldername.startswith(blacklist):
+                filename = foldername + "/" + filename 
+                
+                with open(os.path.join(filename), 'r') as filedata:
+                    string = filedata.read()
+                    codelist.append(string)
+                    codelist.append('#' + '_~_|_~_|'*10)  
+                    
+    with open(newfile, 'w') as filehandle:
+        for listitem in codelist:
+            filehandle.write('%s\n' % listitem)
+    
+    if mdfile:
+        mdfile = newfile.replace("txt", "md")
+        copyfile(newfile, output_path + "/" +  mdfile)
+        os.remove(newfile)
+        return mdfile
+    else:
+        copyfile(newfile, output_path + "/" + newfile)
+        return newfile
